c5039a3609a11a0b3bca158ae5ccf99de06966b8
apperuity <info@apperuity.net>
2017-04-27 18:17:30 +0500

    View Fixed

--- resources/views/estimate/intermediate.blade.php
+++ resources/views/estimate/intermediate.blade.php
@@ -1,55 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="wrapper" ng-controller="intermediate">
-    <div class="container">
-        <div class="table-responsive col-md-8">
-            <table class="table table-bordered ">
-                <thead>
-                <tr>
-                    <td>Total Effort Required</td>
-                    <td><span id="effort"></span>(SLOC)</td>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Total Development Time</td>
-                    <td><span id="develop"></span>0.0 (PM)</td>
-                   </tr>
-                <tr>
-                    <td>Staff Required</td>
-                    <td><span id="staff"></span>0.0 (PM)</td>
-                </tr>
-                </tbody>
 
-            </table>
-        </div>
-        <div class="table-responsive col-md-4">
-            <table class="table table-bordered ">
-
-                <thead>
-                <tr>
-                    <td>Mode</td>
-                    <td><select ng-model="class" ng-init="class='O'" ng-change="doCalculation()">
-                            <option value="O">Organic</option>
-                            <option value="S">SEMIDETACHED</option>
-                            <option value="E">EMBEDDED</option>
-                        </select></td>
-                </tr>
-                <tr>
-                    <th>Average Salary</th>
-                    <th><input size="4" type="text" ng-model="salary" ng-init="salary=1000" ng-change="changeCost()"/> (PM)</th>
-                </tr>
-                <tr>
-                    <th><h3>Total Cost</h3></th>
-                    <th><h3><span id="cost"></span>$</h3>   </th>
-                </tr>
-                </thead>
-            </table>
-        </div>
-        <!-- fieldset collapse start -->
-
-
-    </div>
     <div class="table-responsive col-md-12">
         <h3>COCOMO II Cost Drivers </h3>
         <div class="col-md-4">
@@ -246,6 +198,47 @@
                     </div>
                 </div>
             </fieldset>
+        </div>
+        <div class="col-md-4">
+            <table class="table table-bordered" style="width: 80%;">
+                <thead>
+                <tr>
+                    <td>Total Effort Required</td>
+                    <td><span id="effort"></span>(SLOC)</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Total Development Time</td>
+                    <td><span id="develop"></span>0.0 (PM)</td>
+                </tr>
+                <tr>
+                    <td>Staff Required</td>
+                    <td><span id="staff"></span>0.0 (PM)</td>
+                </tr>
+                </tbody>
+
+            </table>
+            <table class="table table-bordered "  style="width: 80%;">
+             <thead>
+                <tr>
+                    <td>Mode</td>
+                    <td><select ng-model="class" ng-init="class='O'" ng-change="doCalculation()">
+                            <option value="O">Organic</option>
+                            <option value="S">SEMIDETACHED</option>
+                            <option value="E">EMBEDDED</option>
+                        </select></td>
+                </tr>
+                <tr>
+                    <th>Average Salary</th>
+                    <th><input size="4" type="text" ng-model="salary" ng-init="salary=1000" ng-change="changeCost()"/> (PM)</th>
+                </tr>
+                <tr>
+                    <th><h3>Total Cost</h3></th>
+                    <th><h3>$<span id="cost"></span></h3>   </th>
+                </tr>
+                </thead>
+            </table>
         </div>
 
 
